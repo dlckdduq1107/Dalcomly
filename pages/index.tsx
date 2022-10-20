f02983cd662d9039782e1ce00478696c47500e44
dlckdduq1107 <dlckdduq97@gmail.com>
2022-10-21 00:27:27 +0900

    chore: next폴더 구조를 맞추기 위한 파일 이동 및 추가
    _app.tsx는 react에서의 app.tsx같은 역할을 한다.
    next는 src안에 들어가있지 않는다.

---
+++ pages/index.tsx
@@ -0,0 +1,4 @@
+function HomePage() {
+  return <h1>Welcome to Next.js!</h1>;
+}
+export default HomePage;
--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-const App: React.FC = () => {
-  return <div className='main'>hello world</div>;
-};
-
-export default App;
