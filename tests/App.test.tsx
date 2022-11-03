1289b8f5f4dc3ddd95ffa93ee9cb0502264a98f8
dlckdduq1107 <dlckdduq97@gmail.com>
2022-11-03 23:54:43 +0900

    test: 상품 조회 페이지 이미지 렌더링 테스트

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import HomePage from '../pages/index';
+import ProductPage from '../pages/products';
 
-it('Welcome message를 보여준다.', () => {
+it('홈페이지를 보여준다.', () => {
   render(<HomePage />);
   const element = screen.getByRole('link', {
     name: '전체 물품 리스트',
   });
   expect(element).toBeInTheDocument();
+});
+
+it('Product Page를 보여준다.', () => {
+  const productList = {
+    result: [
+      { productName: 'clothes1', imgPath: '/assets/images/metaRabbit.png' },
+      { productName: 'hat', imgPath: '/assets/images/testImg2.png' },
+    ],
+  };
+  render(<ProductPage data={productList} />);
+  const element = screen.getAllByRole('img');
+  expect(element).toHaveLength(2);
 });
