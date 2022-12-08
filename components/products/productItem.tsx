e3422abfca901b69e945064ac30b2a8aa6e34b01
dlckdduq1107 <dlckdduq97@gmail.com>
2022-12-08 23:29:22 +0900

    fix: Link 관련 콘솔 에러 해결
    link컴포넌트의 children에 컴포넌트를 넣었을때 에러를 a태그로 감싸 해결

--- components/products/productItem.tsx
+++ components/products/productItem.tsx
@@ -6,13 +6,15 @@ function ProductItem(props: any) {
   const { imgPath, id, productName } = props;
   return (
     <Link href={`/product/${id}`}>
-      <ProductItemWrapper>
-        <Image src={imgPath} width={100} height={100} />
-        <ItemWrapper>
-          <div>{productName}</div>
-          <div>간단 설명</div>
-        </ItemWrapper>
-      </ProductItemWrapper>
+      <a>
+        <ProductItemWrapper>
+          <Image src={imgPath} width={100} height={100} />
+          <ItemWrapper>
+            <div>{productName}</div>
+            <div>간단 설명</div>
+          </ItemWrapper>
+        </ProductItemWrapper>
+      </a>
     </Link>
   );
 }
