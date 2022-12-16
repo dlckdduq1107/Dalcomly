7ca5cb83f3da439a6dc80fdea4eb910edcb30184
dlckdduq1107 <dlckdduq97@gmail.com>
2022-12-16 09:06:22 +0900

    fix: getServerSide사용
    product디테일 페이지는 동적인 페이지이고 얼마나 많은 상품이 있을지 모르기때문에 요청시에만 서버사이드 렌더링을 하는 getServerSideProps가 좋을것 같음

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -5,27 +5,10 @@ function ProductDetailPage(props: any) {
   return <ProductItem imgPath={imgPath} id={id} productName={productName} />;
 }
 
-export async function getStaticPaths() {
-  const reponse = await fetch('http://localhost:3000/api/products');
-  const result = await reponse.json();
-  const productPath = result.productList.map((val: any) => {
-    return {
-      params: {
-        id: val.id.toString(),
-      },
-    };
-  });
-  return {
-    paths: [...productPath],
-    fallback: 'blocking',
-  };
-}
-
-export async function getStaticProps(props: any) {
+export async function getServerSideProps(props: any) {
   const { id } = props.params;
   const data = await fetch(`http://localhost:3000/api/product/${id}`);
   const result = await data.json();
-  console.log(result);
   return {
     props: {
       id: result.id,
