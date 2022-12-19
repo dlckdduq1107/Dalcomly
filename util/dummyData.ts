04c35e1446a5c1fa03d1e5ef31a4c0809b375d6b
dlckdduq1107 <dlckdduq97@gmail.com>
2022-12-20 07:54:09 +0900

    chore: 더미데이터 수정

--- util/dummyData.ts
+++ util/dummyData.ts
@@ -1,6 +1,18 @@
 export const products = {
   productList: [
-    { id: 1, productName: 'clothes1', imgPath: '/assets/images/metaRabbit.png', price: 10000 },
-    { id: 2, productName: 'hat', imgPath: '/assets/images/testImg2.png', price: 2000 },
+    {
+      id: 1,
+      productName: 'clothes1',
+      imgPath: '/assets/images/metaRabbit.png',
+      price: 10000,
+      cautionPath: '/assets/images/testImg2.png',
+    },
+    {
+      id: 2,
+      productName: 'hat',
+      imgPath: '/assets/images/testImg2.png',
+      price: 2000,
+      cautionPath: '/assets/images/metaRabbit.png',
+    },
   ],
 };
