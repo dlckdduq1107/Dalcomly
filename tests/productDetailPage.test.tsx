61367cb98ca3fd77311c3a625d9b2f8b5c424474
dlckdduq1107 <dlckdduq97@gmail.com>
2022-12-21 08:57:51 +0900

    test: 상품이름, 가격,이미지,구매버튼,장바구니버튼 존재 테스트

--- tests/productDetailPage.test.tsx
+++ tests/productDetailPage.test.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ProductDetailPage from '../pages/product/[id]';
-import { productsResponse } from './apiMockResponse';
+import { products } from '../util/dummyData';
 
-it('Product Detail Page에서 상품 상세정보를 보여준다.', () => {
-  render(<ProductDetailPage {...productsResponse.productList[0]} />);
-  const element = screen.getByText(productsResponse.productList[0].productName);
+beforeEach(() => {
+  render(<ProductDetailPage {...products.productList[0]} />);
+});
+
+it('Product Detail Page - 상품 이름을 보여준다.', () => {
+  const element = screen.getByText(products.productList[0].productName);
   expect(element).toBeInTheDocument();
+});
+
+it('Product Detail Page - 가격을 보여준다', () => {
+  const krPrice = products.productList[0].price.toLocaleString('kr') + '원';
+  const element = screen.getByText(krPrice);
+  expect(element).toBeInTheDocument();
+});
+
+it('Product Detail Page - 상품 이미지를 보여준다', () => {
+  const element = screen.getByAltText('detail-img');
+  expect(element).toBeInTheDocument();
+});
+
+it('Product Detail Page - 구매버튼 존재', () => {
+  const buyBtn = screen.getByRole('buy-btn');
+  expect(buyBtn).toBeInTheDocument();
+});
+
+it('Product Detail Page - 장바구니 버튼 존재', () => {
+  const cartBtn = screen.getByRole('cart-btn');
+  expect(cartBtn).toBeInTheDocument();
 });
