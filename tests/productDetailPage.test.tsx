367874a66d100ab30507b2a6691fa96b2e0ac755
dlckdduq1107 <dlckdduq97@gmail.com>
2022-12-21 23:30:50 +0900

    test: 상품 디테일 페이지 캐러셀 이미지 테스트

--- tests/productDetailPage.test.tsx
+++ tests/productDetailPage.test.tsx
@@ -43,3 +43,43 @@ it('Product Detail Page - 상품 디테일 클릭시 정보 변경(상세정보 
   userEvent.click(cautionBtn);
   expect(await screen.findByAltText('cautionInfo')).toBeInTheDocument();
 });
+
+describe('상품 이미지 캐러셀 테스트', () => {
+  it('다음 버튼 클릭시 두번째 이미지로 변경', async () => {
+    const firstImg = screen.getAllByAltText('img-1');
+    expect(firstImg).toBeInTheDocument();
+    expect(screen.getByAltText('img-2')).not.toBeInTheDocument();
+
+    const nextBtn = screen.getByRole('next-img-btn');
+    userEvent.click(nextBtn);
+    expect(await screen.findByAltText('img-2')).toBeInTheDocument();
+  });
+  it('두번째 이미지에서 이전 버튼 클릭시 첫번째 이미지로 변경', async () => {
+    const nextBtn = screen.getByRole('next-img-btn');
+    userEvent.click(nextBtn);
+    expect(await screen.findByAltText('img-2')).toBeInTheDocument();
+    expect(await screen.findByAltText('img-1')).not.toBeInTheDocument();
+
+    const prevBtn = screen.getByRole('prev-img-btn');
+    userEvent.click(prevBtn);
+    expect(await screen.findByAltText('img-1')).toBeInTheDocument();
+  });
+  it('처음 이미지에서 이전 버튼 클릭시 마지막 이미지로 변경', async () => {
+    const firstImg = screen.getAllByAltText('img-1');
+    expect(firstImg).toBeInTheDocument();
+    expect(screen.getByAltText('img-5')).not.toBeInTheDocument();
+
+    const prevBtn = screen.getByRole('prev-img-btn');
+    userEvent.click(prevBtn);
+
+    expect(await screen.findByAltText('img-5')).toBeInTheDocument();
+  });
+  it('마지막 이미지에서 다음 버튼 클릭시 처음 이미지로 변경', async () => {
+    expect(screen.getByAltText('img-5')).not.toBeInTheDocument();
+
+    const prevBtn = screen.getByRole('prev-img-btn');
+    userEvent.click(prevBtn);
+
+    expect(await screen.findByAltText('img-5')).toBeInTheDocument();
+  });
+});
