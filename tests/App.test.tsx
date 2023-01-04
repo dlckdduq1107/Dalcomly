c6ca7be0ea5e17e697b90dcfe0c93a4c44e112c8
dlckdduq1107 <dlckdduq97@gmail.com>
2023-01-05 00:16:09 +0900

    test: 메인 페이지 prop변경에 따른 테스트 코드 수정

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import HomePage from '../pages/index';
-import { products } from '../util/dummyData';
+import { mainPageImages, products } from '../util/dummyData';
 
 it('메인 페이지를 보여준다.', () => {
-  render(<HomePage {...products} />);
+  render(<HomePage {...products} {...mainPageImages} />);
   const todayHotElement = screen.getByText('금일 hot');
   expect(todayHotElement).toBeInTheDocument();
 
