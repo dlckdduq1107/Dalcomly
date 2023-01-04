7d80b479b3df8f4820fe19081d15fb34003bab7c
dlckdduq1107 <dlckdduq97@gmail.com>
2023-01-05 00:09:22 +0900

    feat: 메인 페이지에 캐러셀 자동 슬라이드 추가 및 이미지 fetch 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Carousel from '../components/carousel';
 import ProductList from '../components/products/productList';
 import { ProductResponse } from './api/products';
 
@@ -8,9 +9,10 @@ interface ProductResponseWrapper {
 
 //TODO: props타입을 설정해야함
 function HomePage(props: any) {
-  const { productList } = props;
+  const { productList, imgPaths } = props;
   return (
     <BodyWrapper>
+      <Carousel imgPaths={imgPaths} width={1425} height={445} kind='main' delay={3000} />
       <ProductList productList={productList} title='금일 hot' />
       <ProductList productList={productList} title='weekly hot items' />
     </BodyWrapper>
@@ -19,10 +21,12 @@ function HomePage(props: any) {
 export async function getStaticProps() {
   const result = await fetch('http://localhost:3000/api/products');
   const { productList } = await result.json();
-
+  const mainImages = await fetch('http://localhost:3000/api/main/images');
+  const { imgPaths } = await mainImages.json();
   return {
     props: {
       productList,
+      imgPaths,
     },
   };
 }
