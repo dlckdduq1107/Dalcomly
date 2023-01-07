2bba78efdc6e6147e7c5fdeb4e780c641a630351
dlckdduq1107 <dlckdduq97@gmail.com>
2023-01-07 15:47:41 +0900

    test: 메인 페이지 캐러셀 자동 슬라이드 테스트
    3초후 다음 이미지로 변경

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import HomePage from '../pages/index';
 import { mainPageImages, products } from '../util/dummyData';
 
@@ -10,4 +10,19 @@ it('메인 페이지를 보여준다.', () => {
 
   const weeklyHotElement = screen.getByText('weekly hot items');
   expect(weeklyHotElement).toBeInTheDocument();
+});
+
+it('메인 페이지 캐러셀 자동 슬라이드 - 3초후 다음 이미지 표시', async () => {
+  render(<HomePage {...products} {...mainPageImages} />);
+
+  const firstImg = screen.getByAltText('img-0');
+  expect(firstImg).toHaveClass('active');
+
+  await waitFor(
+    () => {
+      const secondImg = screen.getByAltText('img-1');
+      expect(secondImg).toHaveClass('active');
+    },
+    { timeout: 3000 }
+  );
 });
