5d19163f611688b852b9a480ba9548706b43f3dc
dlckdduq1107 <dlckdduq97@gmail.com>
2023-01-19 00:30:44 +0900

    fix: 메인 페이지 DB연결 및 api reponse 값 수정

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { products } from '../../util/dummyData';
+import { PrismaClient } from '@prisma/client';
 
 export interface ProductResponse {
   productList: Array<EachProduct>;
@@ -8,9 +8,50 @@ export interface ProductResponse {
 export interface EachProduct {
   id: number;
   productName: string;
-  imgPath: string;
+  mainImagePath: string;
+  detailImagePath: string;
+  cautionPath: string;
+  carouselImages: Array<string>;
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<ProductResponse>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ProductResponse>) {
+  const prisma = new PrismaClient();
+  const product = await prisma.product.findMany();
+  const result = await Promise.all(
+    product.map(async ({ id, name, price }) => {
+      const images = await prisma.image.findMany({
+        where: { productid: id },
+      });
+
+      let mainImage = '';
+      let detailImage = '';
+      let cautionImage = '';
+      const carouselImages: Array<string> = [];
+      images.forEach((val) => {
+        if (val.kind === 'main') {
+          mainImage = val.path as string;
+        } else if (val.kind === 'detail') {
+          detailImage = val.path as string;
+        } else if (val.kind === 'caution') {
+          cautionImage = val.path as string;
+        } else if (val.kind === 'carousel') {
+          carouselImages.push(val.path as string);
+        }
+      });
+
+      return {
+        id,
+        productName: name as string,
+        price,
+        mainImagePath: mainImage,
+        detailImagePath: detailImage,
+        cautionPath: cautionImage,
+        carouselImages,
+      };
+    })
+  );
+
+  await prisma.$disconnect();
+  const products = { productList: result };
   res.status(200).json(products);
 }
