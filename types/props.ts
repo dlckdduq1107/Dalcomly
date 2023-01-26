2cd223ed7bb2da1aef2c5f6a0f3fd1ded27966d8
dlckdduq1107 <dlckdduq97@gmail.com>
2023-01-27 08:47:41 +0900

    refactor: 메인 홈페이지 props 타입 설정 및 추가

---
+++ types/props.ts
@@ -0,0 +1,14 @@
+export interface homeProps {
+  productList: Array<EachProduct>;
+  imgPaths: Array<string>;
+}
+
+export interface EachProduct {
+  id: number;
+  productName: string;
+  price: number;
+  mainImagePath: string;
+  detailImagePath: string;
+  cautionImagePath: string;
+  carouselImages: Array<string>;
+}
