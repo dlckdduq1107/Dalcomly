18e32bb2c2787e9b7c77a6e743f1760284d33d6f
dlckdduq1107 <dlckdduq97@gmail.com>
2023-02-03 07:49:43 +0900

    fix: 메인 페이지 캐러셀 이미지 api 백엔드 연결

--- pages/api/main/images.ts
+++ pages/api/main/images.ts
@@ -1,8 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { MainPageCarouselImages } from '../../../types/props';
 import { mainPageImages } from '../../../util/dummyData';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<MainPageCarouselImages>) {
-  res.status(200).json(mainPageImages);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<MainPageCarouselImages>
+) {
+  const prisma = new PrismaClient();
+  const resHomeImages = await prisma.homeImage.findMany();
+
+  const result = {
+    imgPaths: resHomeImages.map((eachPath) => eachPath.path),
+  };
+  res.status(200).json(result);
 }
